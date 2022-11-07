12f2bd916042fe16364ed7add6595e88f88ab5fe
Anthony Costanzo <38962968+AnthonyCostanzo@users.noreply.github.com>
2022-11-06 19:47:30 -0500

    Update OrderItem.java

--- src/main/java/com/warehouse/warehouseproject/model/OrderItem.java
+++ src/main/java/com/warehouse/warehouseproject/model/OrderItem.java
@@ -2,50 +2,49 @@ package com.warehouse.warehouseproject.model;
 
 import com.sun.istack.NotNull;
 
-//import javax.persistence.*;
-//import java.util.Date;
-//
-//@Entity
-//@Table(name = "orderitems")
-//public class OrderItem {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer orderItemId;
-//
-//    @Column(name = "productId")
-//    private @NotNull Long productId;
-//
-//    @Column(name = "quantity")
-//    private @NotNull int quantity;
-//
-//    @Column(name = "price")
-//    private @NotNull double price;
-//
-//    @Column(name = "order_id")
-//    private Integer orderId;
-//
-//    @Column(name = "created_date")
-//    private Date createdDate;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "order_id",referencedColumnName = "order_id",insertable = false,updatable = false)
-//    private Order order;
-//
-//    @OneToOne
-//    @JoinColumn(name = "item",referencedColumnName = "id",insertable = false,updatable = false)
-//    private Item item;
-//
-//    public OrderItem(){}
-//
-//    public OrderItem(Integer orderId, @NotNull Long product_id, @NotNull int quantity, @NotNull double price) {
-//        this.productId = product_id;
-//        this.quantity = quantity;
-//        this.price = price;
-//        this.orderId=orderId;
-//        this.createdDate = new Date();
-//    }
-//    //getter and setter are removed for sake of brevity
-//
-//}
-//
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "orderitems")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @Column(name = "item_id")
+    private @NotNull Long item_id;
+
+    @Column(name = "quantity")
+    private @NotNull int quantity;
+
+    @Column(name = "price")
+    private @NotNull double price;
+
+    @Column(name = "order_id")
+    private Long order_id;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @ManyToOne
+    private Order order;
+
+    @OneToOne
+    @JoinColumn(name = "item",referencedColumnName = "id",insertable = false,updatable = false)
+    private Item item;
+
+    public OrderItem(){}
+
+    public OrderItem(Long orderId, @NotNull Long product_id, @NotNull int quantity, @NotNull double price) {
+        this.item_id = product_id;
+        this.quantity = quantity;
+        this.price = price;
+        this.order_id=orderId;
+        this.createdDate = new Date();
+    }
+
+}
+
+
